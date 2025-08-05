ec9e1a103f60db0e23c14b66cca75499ab351b67
ricksonl.berigo <ricksonl.berigo@outlook.com>
2025-08-05 14:56:29 -0300

    Refactor step 3 view: update payment method section and enhance layout for better clarity and usability.

--- resources/views/deliquencies/steps/step3.blade.php
+++ resources/views/deliquencies/steps/step3.blade.php
@@ -1,10 +1,7 @@
 <form action="" method="POST" class="needs-validation" novalidate>
     <!-- Account Details -->
     <div id="account-details" class="content active dstepper-block">
-        <div
-            class="content-header mb-4 bg-light p-3"
-            style="border-radius: 0.5rem"
-        >
+        <div class="content-header bg-light mb-4 p-3" style="border-radius: 0.5rem">
             <div class="d-flex mb-2">
                 <div class="d-flex align-items-center gap-2">
                     <h6 class="mb-0">Fiança disponível:</h6>
@@ -18,71 +15,33 @@
             </p>
         </div>
 
-        <!--
         <div class="row g-6">
             <div class="content-header mb-4">
                 <div class="card-header">
-                    <h4>Sobre o imóvel</h4>
+                    <h4>Forma de pagamento</h4>
                     <p>
-                        Precisamos de algumas informações sobre a situação do
-                        imóvel para garantir a análise da inadimplência.
+                        Escolha como gostaria de receber o pagamento da Invicta
                     </p>
                 </div>
 
                 <div class="card-body">
                     <div class="row">
                         <div class="col-md mb-md-0 mb-5">
-                            <div
-                                class="form-check custom-option custom-option-basic"
-                            >
-                                <label
-                                    class="form-check-label custom-option-content"
-                                    for="customRadioTemp1"
-                                >
-                                    <input
-                                        name="customRadioTemp"
-                                        class="form-check-input"
-                                        type="radio"
-                                        value=""
-                                        id="customRadioTemp1"
-                                        checked=""
-                                    />
+                            <div class="form-check custom-option custom-option-basic">
+                                <label class="form-check-label custom-option-content" for="customRadioTemp1">
+                                    <input name="customRadioTemp" class="form-check-input" type="radio" value=""
+                                        id="customRadioTemp1" checked="" />
                                     <span class="custom-option-header">
-                                        <span class="h6 mb-0">Ocupado</span>
+                                        <span class="h6 mb-0">TED</span> <span
+                                            class="badge bg-label-success me-4 rounded p-2">Recomendado</span>
                                     </span>
                                     <span class="custom-option-body">
-                                        <small
-                                            >O inquilino ainda esta no
-                                            imóvel</small
-                                        >
-                                    </span>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div
-                                class="form-check custom-option custom-option-basic"
-                            >
-                                <label
-                                    class="form-check-label custom-option-content"
-                                    for="customRadioTemp2"
-                                >
-                                    <input
-                                        name="customRadioTemp"
-                                        class="form-check-input"
-                                        type="radio"
-                                        value=""
-                                        id="customRadioTemp2"
-                                    />
-                                    <span class="custom-option-header">
-                                        <span class="h6 mb-0">Desocupado</span>
-                                    </span>
-                                    <span class="custom-option-body">
-                                        <small
-                                            >O inquilino já saiu do imóvel porém
-                                            ainda tem débitos com a
-                                            imobiliária</small
-                                        >
+                                        <small>Beneficiário: </small> <br>
+                                        <small>CNPJ/CPF: </small> <br>
+                                        <small>Banco: </small> <br>
+                                        <small>Agência: </small> <br>
+                                        <small>Conta: </small> <br>
+                                        <small>Tipo de Conta: </small>
                                     </span>
                                 </label>
                             </div>
@@ -91,17 +50,36 @@
                 </div>
             </div>
 
+            <hr class="my-0 py-0">
+
+            <div class="content-header mb-4">
+                <div class="card-header">
+                    <h4>Resumo</h4>
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md mb-md-0 mb-5">
+                            <div class="card">
+                                <small>Tipo</small>
+                                <h3>R$ 3.000,00</h3>
+                                <small>Valor</small>
+                                <h3>Valores comunicados</h3>
+                            </div>
+                        </div>
+
+                        <div class="col-12">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="col-12 d-flex justify-content-end">
-                <button
-                    type="submit"
-                    class="btn btn-primary btn-next waves-effect waves-light"
-                >
-                    <span class="align-middle d-sm-inline-block d-none me-sm-2"
-                        >Próximo</span
-                    >
+                <button type="submit" class="btn btn-primary btn-next waves-effect waves-light">
+                    <span class="d-sm-inline-block d-none me-sm-2 align-middle">Próximo</span>
                     <i class="icon-base ti tabler-arrow-right icon-xs"></i>
                 </button>
             </div>
         </div>
-    --></div>
+    </div>
 </form>
